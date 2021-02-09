241edd13cef86a31b438175fd2533323d35d9063
melanie971 <melanie.beguer@gmail.com>
2021-02-09 10:50:31 +0100

    Updates

---
+++ precache-manifest.610789a2964d9b5346c99bcb95e8f0cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36e40b7196a1eef5e4cf01b59fa6b219",
+    "url": "/DappWhiteListDeployed/index.html"
+  },
+  {
+    "revision": "b3ccf032d2ac3de5e581",
+    "url": "/DappWhiteListDeployed/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "6884af4947f4f7377018",
+    "url": "/DappWhiteListDeployed/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b3ccf032d2ac3de5e581",
+    "url": "/DappWhiteListDeployed/static/js/2.8e48020b.chunk.js"
+  },
+  {
+    "revision": "6884af4947f4f7377018",
+    "url": "/DappWhiteListDeployed/static/js/main.cd7da1ad.chunk.js"
+  },
+  {
+    "revision": "b2aeb1ce82dce640afd1",
+    "url": "/DappWhiteListDeployed/static/js/runtime-main.61241c7c.js"
+  }
+]);
